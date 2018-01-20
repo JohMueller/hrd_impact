c94179f3f0ccdd5b121a3a39495fd79fdd902a28
JohMueller <johannes.m@correlaid.org>
2018-01-20 15:51:57 +0100

    add second predictor

--- ana_causalimpact.R
+++ ana_causalimpact.R
@@ -3,6 +3,8 @@
 
 tweets_amnesty <- create_tweet_df_from_json("amnesty-tweets.json")
 tweets_wwf <- create_tweet_df_from_json("wwf-tweets.json")
+tweets_unaids <- create_tweet_df_from_json("UNAIDS-tweets.json")
+
 
 ts_amnesty <-create_timesseries_df_from_tweet_df(tweets_amnesty,
                                                  include_retweets = TRUE,
@@ -14,9 +16,15 @@ ts_wwf <-create_timesseries_df_from_tweet_df(tweets_wwf,
                                              min_date = "2017-10-10",
                                              max_date = "2018-01-10")
 
+ts_unaids <-create_timesseries_df_from_tweet_df(tweets_unaids,
+                                             include_retweets = TRUE,
+                                             min_date = "2017-10-10",
+                                             max_date = "2018-01-10")
+
 
 causalimpact_df <- as.data.frame(cbind(ts_amnesty$total_engagement,
-                                       ts_wwf$total_engagement))
+                                       ts_wwf$total_engagement,
+                                       ts_unaids$total_engagement))
 
 matplot(causalimpact_df,  type = "l")
 
